2eb5abd580e1182e32ca1e0851e6b4c740b59171
Kieran Miller <kieran.miller@emerson.com>
2023-04-20 19:24:34 -0400

    Adding to the ScheduledFlights cascades to the Flights table

--- air-3550/Repositories/FlightRepository.cs
+++ air-3550/Repositories/FlightRepository.cs
@@ -35,5 +35,53 @@ namespace air_3550.Repositories
             }
         }
 
+
+        public void DeleteByID(int flightID)
+        {
+            try
+            {
+                using (SqliteConnection connection = new SqliteConnection(connectionString))
+                {
+                    connection.Execute("DELETE FROM Flights WHERE FlightID = @flightID", new { flightID = flightID });
+                }
+            }
+            catch (SqliteException sqlEx)
+            {
+                Logger.LogException(sqlEx);
+            }
+        }
+
+        public void DeleteByScheduledFlightID(int scheduledFlightID)
+        {
+            try
+            {
+                using (SqliteConnection connection = new SqliteConnection(connectionString))
+                {
+                    connection.Execute("DELETE FROM Flights WHERE ScheduledFlightID = @scheduledFlightID", new { scheduledFlightID = scheduledFlightID });
+                }
+            }
+            catch (SqliteException sqlEx)
+            {
+                Logger.LogException(sqlEx);
+            }
+        }
+
+        public void InsertFlights(List<Flight> flights)
+        {
+            try
+            {
+                using (SqliteConnection connection = new SqliteConnection(connectionString))
+                {
+                    string sql = "INSERT INTO Flights (ScheduledFlightID, DepartureDate, ArrivalDate, EmptySeats) " +
+                                 "VALUES (@ScheduledFlightID, @DepartureDate, @ArrivalDate, @EmptySeats)";
+                    connection.Execute(sql, flights); 
+                }
+            }
+            catch (SqliteException sqlEx)
+            {
+                Logger.LogException(sqlEx);
+            }
+        }
+
     }
 }
--- air-3550/Repositories/ScheduledFlightRepository.cs
+++ air-3550/Repositories/ScheduledFlightRepository.cs
@@ -1,4 +1,5 @@
-﻿using air_3550.Logging;
+﻿using air_3550.Database;
+using air_3550.Logging;
 using air_3550.Models;
 using Dapper;
 using Microsoft.Data.Sqlite;
@@ -34,7 +35,7 @@ namespace air_3550.Repositories
                 return null;
             }
         }
-
+        // When we add a scheduled flight, trigger a cascade to the Flights table that adds the same scheduled flight at the same time each day for the next 6 months.
         public void Add(ScheduledFlight scheduledFlight)
         {
             try
@@ -43,14 +44,70 @@ namespace air_3550.Repositories
                 {
                     string sql =
                        "INSERT INTO ScheduledFlights (OriginAirportID, DestinationAirportID, AircraftID, DepartureTime, ArrivalTime, Distance) " +
-                       "VALUES (@OriginAirportID, @DestinationAirportID, @AircraftID, @DepartureTime, @ArrivalTime, @Distance)";
-                    connection.Execute(sql, scheduledFlight);
+                       "VALUES (@OriginAirportID, @DestinationAirportID, @AircraftID, @DepartureTime, @ArrivalTime, @Distance); " +
+                       "SELECT last_insert_rowid()";
+
+                    scheduledFlight.ScheduledFlightID = connection.QuerySingle<int>(sql, scheduledFlight);
+                    List<Flight> flightsToAdd = generateFlightsForSixMonths(scheduledFlight);
+                    DatabaseManager.Instance.Flights.InsertFlights(flightsToAdd);
                 }
+
             }
             catch (SqliteException sqlEx)
             {
                 Logger.LogException(sqlEx);
             }
+        }
+        // When we delete a scheduled flight, cascade the changes to the Flights table 
+        public void DeleteByID(int scheduledFlightID)
+        {
+            try
+            {
+                using (SqliteConnection connection = new SqliteConnection(connectionString))
+                {
+                    string sql =
+                       "DELETE FROM ScheduledFlights WHERE ScheduledFlightID = @scheduledFlightID";
+                    connection.Execute(sql, new { scheduledFlightID = scheduledFlightID });
+                }
+                DatabaseManager.Instance.Flights.DeleteByScheduledFlightID(scheduledFlightID);
+            }
+            catch (SqliteException sqlEx)
+            {
+                Logger.LogException(sqlEx);
+            }
+
+        }
+
+        private List<Flight> generateFlightsForSixMonths(ScheduledFlight scheduledFlight)
+        {
+            return generateFlightsForSixMonths(new List<ScheduledFlight> { scheduledFlight });
+        }
+
+        private List<Flight> generateFlightsForSixMonths(List<ScheduledFlight> scheduledFlights)
+        {
+            List<Flight> flights = new List<Flight>();
+            DateTime startDate = DateTime.Today;
+            DateTime endDate = startDate.AddMonths(6);
+
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                foreach (var scheduledFlight in scheduledFlights)
+                {
+                    DateTime departureDateTime = date.Add(TimeSpan.Parse(scheduledFlight.DepartureTime));
+                    DateTime arrivalDateTime = date.Add(TimeSpan.Parse(scheduledFlight.ArrivalTime));
+
+                    Flight flight = new Flight
+                    {
+                        ScheduledFlightID = scheduledFlight.ScheduledFlightID,
+                        DepartureDate = departureDateTime.ToShortDateString(),
+                        ArrivalDate = arrivalDateTime.ToShortDateString(),
+                        EmptySeats = null
+                    };
+                    flights.Add(flight);
+                }
+            }
+
+            return flights;
         }
     }
 }
